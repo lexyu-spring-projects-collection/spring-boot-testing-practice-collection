27f3c40e547ae993f0738974911cacb2ceb29d29
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-02-24 18:31:05 +0800

    Learning JUnit 5 & Mockito & WebMvcTest

--- spring-boot-unit-testing/src/test/java/com/lex/practice/SpringBootUnitTestingApplicationTests.java
+++ spring-boot-unit-testing/src/test/java/com/lex/practice/SpringBootUnitTestingApplicationTests.java
@@ -3,7 +3,7 @@ package com.lex.practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // Good at Integration Test
 class SpringBootUnitTestingApplicationTests {
 
     @Test
--- spring-boot-unit-testing/src/test/java/com/lex/practice/controller/BookControllerTest2.java
+++ spring-boot-unit-testing/src/test/java/com/lex/practice/controller/BookControllerTest2.java
@@ -3,25 +3,25 @@ package com.lex.practice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lex.practice.dao.BookRepository;
 import com.lex.practice.entity.Book;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,18 +29,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author : LEX_YU
  * @version : 0.0.1
  * @date : 2023/2/23 下午 02:06
+ * Ref Spring Boot Testing Doc
+ * `@WebMvcTest + @MockBean`
  */
-//@RunWith(MockitoJUnitRunner.class) //JUnit 4, enable Mockito annotation
-@ExtendWith(MockitoExtension.class) // JUnit 5, equivalent above
-class BookControllerTest {
+//@DataJpaTest // Test Persistence Layer
+@WebMvcTest(BookController.class) // Test slice of an application
+class BookControllerTest2 {
 
+    @Autowired
     private MockMvc mockMvc;
+    @Autowired
     private ObjectMapper mapper;
-    private AutoCloseable closeable;
-    @InjectMocks
-    private BookController bookController;
-    @Mock
-    BookRepository bookRepository;
+
+    @MockBean
+    private BookRepository bookRepository;
 
     List<Book> books = List.of(
             new Book(1L, "A Book", "A summary", 5),
@@ -48,19 +50,11 @@ class BookControllerTest {
             new Book(3L, "C Book", "C summary", 3)
     );
 
-    @BeforeEach // JUnit 5
-    public void init() {
-        closeable = MockitoAnnotations.openMocks(this);
-        bookController = new BookController(bookRepository);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
-    }
-
     @Test
     void getAllBookRecords() throws Exception {
-        when(bookRepository.findAll()).thenReturn(books);
+        given(bookRepository.findAll()).willReturn(books);
         mockMvc.perform(get("/book")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andDo(print())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[2].name", is("C Book")))
@@ -68,7 +62,14 @@ class BookControllerTest {
     }
 
     @Test
-    void getBookById() {
+    void getBookById() throws Exception {
+        given(bookRepository.findById(books.get(0).getId()))
+                .willReturn(Optional.ofNullable(books.get(0)));
+        mockMvc.perform(get("/book/1").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is("A Book")))
+                .andReturn();
     }
 
     @Test
@@ -77,10 +78,5 @@ class BookControllerTest {
 
     @Test
     void updateBookRecord() {
-    }
-
-    @AfterEach
-    void closeRepository() throws Exception {
-        closeable.close();
     }
 }
